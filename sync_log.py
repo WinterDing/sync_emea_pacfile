6e86dd307df1e479af3827323cd93f1f881d725d
witner <flyawaywinter@126.com>
2017-09-21 16:57:43 +0800

    first commit

---
+++ sync_log.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = "Winter Ding"
+# Email: flyawaydxd@126.com
+import logging
+from logging import handlers
+import os
+
+log_file = 'C:\Users\z218680\Desktop\pac\synclog\sync_log.txt'
+log_folder = os.path.dirname(log_file)
+if not os.path.exists(log_folder):
+    os.mkdir(log_folder)
+
+current_user = os.environ['USERNAME']
+
+logger = logging.getLogger(current_user)
+logger.setLevel(level=logging.INFO)
+
+rotateLog = handlers.RotatingFileHandler(log_file, maxBytes=10*1024, backupCount=3)
+rotateLog.setLevel(level=logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s-%(name)s-%(levelname)s-%(message)s')
+rotateLog.setFormatter(formatter)
+
+logger.addHandler(rotateLog)
+
+
+
